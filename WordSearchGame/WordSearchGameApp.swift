47cd182917cec6cbe906c653d2fbd23f58a1e953
Kostiantyn Nevinchanyi <nevinchanyi.k@gmail.com>
2024-09-15 15:31:08 -0500

    Initial Commit

---
+++ WordSearchGame/WordSearchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordSearchGameApp.swift
+//  WordSearchGame
+//
+//  Created by Kostiantyn Nevinchanyi on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WordSearchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
